04a38310ba3f454c73fddcd2ae2ade744e3562f0
itolstov <attid0@gmail.com>
2024-07-07 17:11:41 +0200

    Refactored QR code decoding functions to use both OpenCV and Pyzbar libraries for more accurate results. Updated requirements.txt to include Pyzbar library.

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,6 +1,8 @@
 from typing import Union
 from aiogram import types
 import cv2  # opencv-python
+from pyzbar.pyzbar import decode
+from PIL import Image
 
 
 def get_user_id(user_id: Union[types.CallbackQuery, types.Message, int]) -> int:
@@ -14,15 +16,37 @@ def get_user_id(user_id: Union[types.CallbackQuery, types.Message, int]) -> int:
     return user_id
 
 
-def decode_qr_code(image_path):
-    # Загрузка изображения
+def decode_qr_code_cv(image_path):
     image = cv2.imread(image_path)
-
-    # Инициализация детектора QR-кода
     qr_code_detector = cv2.QRCodeDetector()
-
-    # Распознавание и декодирование QR-кода
     decoded_text, points, _ = qr_code_detector.detectAndDecode(image)
 
-    if points is not None:
+    if points is not None and decoded_text:
         return decoded_text
+    else:
+        return None
+
+
+def decode_qr_code_pyzbar(image_path):
+    image = Image.open(image_path)
+    decoded_objects = decode(image)
+    if decoded_objects:
+        return decoded_objects[0].data.decode('utf-8')
+    else:
+        return None
+
+
+def decode_qr_code(image_path):
+    result = decode_qr_code_cv(image_path)
+
+    if result is None:
+        result = decode_qr_code_pyzbar(image_path)
+
+    if result is None:
+        return None
+    else:
+        return result
+
+
+if __name__ == '__main__':
+    pass
