bc1eb75fb20436a285762d698d8bf43a66eff6ae
Artem Kanarev <artem@retechlabs.com>
2024-12-18 19:43:09 +0100

    try to receive address by tg username in bsn editor

--- routers/bsn/__init__.py
+++ routers/bsn/__init__.py
@@ -23,7 +23,7 @@ async def bsn_mode_command(message: "Message", state: "FSMContext", command: "Co
     tags = await bsn_stellar_get_data(session, message.from_user.id)
     if command.args:
         tag = command.args
-        await parse_tag(tag, tags, message)
+        await parse_tag(tag=tag, bsn_data=tags, message=message, session=session)
     await message.reply(
         make_tag_message(tags, message.from_user.id),
         reply_markup=get_bsn_kb(message.from_user.id, not tags.is_empty()),
@@ -33,13 +33,13 @@ async def bsn_mode_command(message: "Message", state: "FSMContext", command: "Co
 
 
 @bsn_router.message(BSNStates.waiting_for_tags)
-async def process_tags(message: "Message", state: "FSMContext"):
+async def process_tags(message: "Message", state: "FSMContext", session: "Session", **kwargs) -> None:
     data = await state.get_data()
     tags = data.get('tags')
     new_tags: list[str] = message.text.split('\n')
     for tag in new_tags:
         tag = tag.strip()
-        await parse_tag(tag, tags, message)
+        await parse_tag(tag=tag, bsn_data=tags, message=message, session=session)
 
     await state.update_data({'tags': tags})
     await message.answer(
